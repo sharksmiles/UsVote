40787389e4e1b3f7b8993de03969610b08ff2531
chenwu <chenwu@skieer.com>
2024-05-29 00:47:25 +0800

    feat: USVOTE init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import _Home from '../pages/Home'
+
+export default function Home() {
+  return (
+    <main>
+      <_Home/>
+    </main>
+  );
+}
---
+++ pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="responsive-text">Home</div>;
+};
+
+export default Home;
